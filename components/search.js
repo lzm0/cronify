8cc05a4c4be1eadc09952ac9c0e00d51f37ea00a
Zimo Li <7163127+lzm0@users.noreply.github.com>
2022-08-03 16:00:49 -0400

    Add icons

--- components/search.js
+++ components/search.js
@@ -38,12 +38,33 @@ export default function Search() {
         className="flex justify-between space-x-2 my-10"
         onSubmit={handleSubmit}
       >
-        <input
-          type="text"
-          className="grow placeholder:italic placeholder:text-slate-400 text-slate-800 text-lg bg-white border border-slate-300 rounded-xl w-10 h-14 px-5 transition shadow-sm focus:outline-none focus:border-pink-500 focus:ring-pink-500 focus:ring-1"
-          onChange={handleChange}
-          placeholder={input}
-        />
+        <div className="grow relative">
+          <input
+            type="text"
+            className="w-full placeholder:italic placeholder:text-slate-400 text-slate-800 text-lg bg-white border border-slate-300 rounded-xl h-14 pl-5 pr-12 transition shadow-sm focus:outline-none focus:border-pink-500 focus:ring-pink-500 focus:ring-1"
+            onChange={handleChange}
+            placeholder={input}
+          />
+          <a
+            href="https://blog.zimo.li/introducing-cronify"
+            title="Learn more"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute inset-y-0 translate-y-1/2 right-0 mr-4 h-7 w-7 fill-slate-300"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+        </div>
         <button
           type="submit"
           className="flex justify-center items-center bg-gradient-to-b from-pink-500 to-red-500 text-pink-100 h-14 min-w-14 px-5 transition shadow-lg hover:shadow-xl shadow-pink-400/40 hover:shadow-pink-400/30 rounded-xl text-lg"
@@ -82,6 +103,15 @@ export default function Search() {
           target="_blank"
         >
           Open in crontab.guru
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 ml-1 inline"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+          </svg>
         </a>
       </div>
     </div>
